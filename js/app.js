94693c99cbcd2cd48811b7f0e42f4ca81027d093
javierWebDev <151965097+JavierWebDev@users.noreply.github.com>
2024-02-29 19:44:56 -0500

    Update

--- js/app.js
+++ js/app.js
@@ -1,128 +1,74 @@
-const HeroesMarvel = [
-    {
-        "id": "5d86371f1efebc31def272e2",
-        "about": "Ipsum duis incididunt ullamco tempor. Amet incididunt Lorem consequat labore culpa. Pariatur amet veniam reprehenderit sunt laborum excepteur. Labore eu ut ut Lorem labore aliqua quis ex elit nulla in incididunt commodo aliquip. Velit excepteur eiusmod Lorem esse officia. Irure aliquip Lorem fugiat voluptate dolor duis consectetur aliquip pariatur tempor reprehenderit deserunt.",
-        "picture": "https://www.sideshow.com/storage/product-images/903421/iron-man_marvel_gallery_5c4cced10da7f.jpg",
-        "name": "Iron Man"
-    },
-    {
-        "id": "5d86371f2343e37870b91ef1",
-        "about": "Mollit officia ad excepteur anim proident incididunt eiusmod mollit consectetur id sit velit. Laborum ut magna officia qui laboris eiusmod do culpa. Culpa dolor officia velit cillum culpa deserunt cupidatat cillum ipsum laborum.",
-        "picture": "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2017/05/hulk-25-curiosidades-gigante-esmeralda-marvel_0.jpg?tf=1200x1200",
-        "name": "Hulk"
-    },
-    {
-        "id": "5d86371f25a058e5b1c8a65e",
-        "about": "Laboris est duis eiusmod adipisicing cillum ut sit ea Lorem non laboris quis Lorem. Est culpa esse aliqua non labore dolor esse labore nulla mollit. Nostrud amet est quis adipisicing dolor. Labore veniam elit veniam non ad ex consequat excepteur eiusmod. Minim cupidatat velit Lorem culpa quis consequat incididunt qui amet incididunt pariatur ex aliquip aliqua. Magna ex elit in aliquip minim eu ut ut fugiat ullamco deserunt adipisicing cillum.",
-        "picture": "https://i.pinimg.com/736x/ba/9b/36/ba9b3623c9a639296e85f7ff09c3c205.jpg",
-        "name": "Captain America"
-    },
-    {
-        "id": "5d86371f9f80b591f499df32",
-        "about": "Ipsum tempor sunt Lorem est. Fugiat nisi velit veniam labore et ullamco minim adipisicing do culpa. Cillum voluptate reprehenderit aute consectetur.",
-        "picture": "http://im.rediff.com/movies/2019/mar/06captain-marvel1.jpg",
-        "name": "Captain Marvel"
-    },
-    {
-        "id": "5d86371f233c9f2425f16916",
-        "about": "Deserunt voluptate aliquip ex dolor Lorem exercitation aliqua nisi fugiat aliquip sunt non. Eu cillum enim velit exercitation officia proident exercitation ipsum exercitation Lorem nulla do. Minim sint dolor nostrud ipsum laborum. Velit ea ad ad consectetur nisi Lorem laborum officia esse. Do eu incididunt eiusmod voluptate excepteur consequat ipsum ad. Quis veniam exercitation eiusmod amet non non eu aliquip quis ea. Tempor deserunt nulla adipisicing qui fugiat ipsum labore duis et ea consectetur.",
-        "picture": "https://m.media-amazon.com/images/M/MV5BYTU4NmRhNTctMjI2ZS00ZTUyLWI1ZjEtZWU1ODAxMjM5MzlmXkEyXkFqcGdeQXVyNDQxNjcxNQ@@._V1_UY1200_CR165,0,630,1200_AL_.jpg",
-        "name": "Black Widow"
-    },
-    {
-        "id": "5d86371f97c29d020f1e1f6d",
-        "about": "Esse magna Lorem ipsum incididunt sit. Enim eiusmod proident in adipisicing anim excepteur laborum magna aliqua adipisicing ad qui aliqua. Eiusmod enim ullamco laboris adipisicing tempor sint labore. Exercitation cupidatat incididunt nostrud velit nisi aute eu ex pariatur deserunt mollit. Deserunt nisi enim irure elit. Aute amet amet consequat laborum dolor sint anim Lorem id fugiat commodo cupidatat ad. Ipsum quis enim consectetur occaecat laboris esse.",
-        "picture": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/4/48/Falcon_AIW_Profile.jpg/revision/latest/scale-to-width-down/310?cb=20180518212822",
-        "name": "Falcon"
-    },
-    {
-        "id": "5d86371fd55e2e2a30fe1ccb",
-        "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/9/99/Black_Panther_AIW_Profile.jpg/revision/latest/scale-to-width-down/310?cb=20180518212436",
-        "name": "Black Panther"
-    },
-    {
-        "id": "5d86371fd55e2e2a30fe1ccb1",
-        "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://www.elfinanciero.com.mx/resizer/4zjPS_QfhwZJXF8CeoHomjD9mb8=/400x267/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/elfinanciero/HARUQVLTIBAGTOZWKJG6LK66XE.jpg",
-        "name": "Doctor Strange"
-    }, {
-        "id": "5d86371fd55e2e2a30fe1ccb2",
-        "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://www.sideshow.com/storage/product-images/903735/spider-man-advanced-suit_marvel_silo.png",
-        "name": "Spider Man"
-    },
-    {
-        "id": "5d86371fd55e2e2a30fe1cc3",
-        "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://fsmedia.imgix.net/f5/ba/d8/66/7dd4/44e9/8e0a/d91348489582/ant-man-and-the-wasp-2018.jpeg#image",
-        "name": "Ant Man"
-    },
-    {
-        "id": "5d86371fd55e2e2a30fe1cc4",
-        "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://hips.hearstapps.com/hmg-prod/images/deadpool-1620403945.jpg?crop=0.529xw:1.00xh;0.248xw,0&resize=1200:*",
-        "name": "Deadpool"
-    },
-    {
-        "id": "9a2b1c4d3e5f6g7h8i9j0k1l2m",
-        "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://i.ebayimg.com/images/g/DugAAOSwphJfKvSN/s-l1200.webp",
-        "name": "QuickSilver"
-    }, {
-        "id": "n3o4p5q6r7s8t9u0v1w2x3y4z5",
-        "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://upload.wikimedia.org/wikipedia/en/5/5d/Wolverine_%28James_%27Logan%27_Howlett%29.png",
-        "name": "Wolverine"
-    },
-    {
-        "id": "6a7b8c9d0e1f2g3h4i5j6k7l8m9n",
-        "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://www.orientaserie.it/wp-content/uploads/2022/10/daredevil-poster.jpg",
-        "name": "Daredevil"
-    },
-    {
-        "id": "o0p1q2r3s4t5u6v7w8x9y0z1a2b3c",
-        "about": "Id aute in dolore dolor in incididunt dolore duis do mollit officia. Ullamco pariatur eiusmod laborum culpa quis non occaecat ad cillum dolore labore aliqua. Elit magna commodo aliquip laborum aliqua duis.",
-        "picture": "https://m.media-amazon.com/images/M/MV5BMjI5Mjg1NDcyOV5BMl5BanBnXkFtZTgwMjAxOTQ5MTI@._V1_.jpg",
-        "name": "Iron Fist"
-    }
-]
-
-const cardOriginalMarvel = document.getElementById('ContHeroCardMarvel')
-const contTarjetasMarvel = document.getElementById('ContTarjetasMarvel')
+let HeroesMarvel = []
+let HeroesDC = []
 
 
-const cardOriginalDC = document.getElementById('ConHeroCardDC')
-const contTarjetasDC = document.getElementById('ContTarjetasDC')
+fetch('../storage/marvel.json')
+    .then(response => response.json())
+    .then(data => {
+        HeroesMarvel = data
+        CrearTarjetasMarvel(data)
+    })
 
-const BtnAbrirModal = document.getElementById('BotonAbrirModal')
-const BtnCerrarrModal = document.getElementById('BtnCerrarModal')
-const modal = document.getElementById('infoMarvel')
+fetch('../storage/dc.json')
+    .then(response => response.json())
+    .then(data => {
+        HeroesDC = data
+        CrearTarjetasDC(data)
+    })
 
-function clonar(original) {
-    let clon = original.cloneNode(true);
-    return clon
+function CrearTarjetasMarvel(data) {
+    const ContenedorHeroesMarvel = document.querySelector("#ContTarjetasMarvel")
+
+    data.forEach(hero => {
+        const clon = document.createElement("div")
+        clon.classList.add("tarjeta")
+        clon.id = "ContHeroCardMarvel"
+        
+
+        clon.innerHTML = `
+
+        <h1 id="TxtTituloMarvel" class="cont-titulo">${hero.name}</h1>
+
+        <div id="ContImgMarvel" class="cont-img_heroe"></div>
+
+        <button class="cont-btn_visualizar" id="${hero.id}">VER</button> 
+
+        `
+
+        clon.querySelector("#ContImgMarvel").style.backgroundImage = `url('${hero.picture}')`
+        ContenedorHeroesMarvel.append(clon)
+    })
 }
 
-function openModal() {
-    
+function CrearTarjetasDC(data) {
+    const ContenedorHeroesDC = document.querySelector("#ContTarjetasDC")
+
+    data.forEach(hero => {
+        const clon = document.createElement("div")
+        clon.classList.add("tarjeta")
+        clon.id = "ContHeroCardDC"
+        
+
+        clon.innerHTML = `
+
+        <h1 id="TxtTituloDC" class="cont-titulo">${hero.name}</h1>
+
+        <div id="ContImgDC" class="cont-img_heroe"></div>
+
+        <button class="cont-btn_visualizar" id="${hero.id}">VER</button> 
+
+        `
+
+        clon.querySelector("#ContImgDC").style.backgroundImage = `url('${hero.picture}')`
+        ContenedorHeroesDC.append(clon)
+    })
+}
+
+const Modal = document.querySelector("#ContModal")
+
+function CloseModal() {
+    Modal.style.display = "none"
 }
 
 
-
-HeroesMarvel.forEach(hero => {
-    
-    let clon = clonar(cardOriginalMarvel)
-
-    clon.querySelector('#TxtTituloMarvel').innerHTML = hero.name
-    clon.querySelector('#ContImgMarvel').style.backgroundImage = `url('${hero.picture}')`
-
-    contTarjetasMarvel.appendChild(clon)
-
-
-
-})
-
-cardOriginalMarvel.style.display = "none"
 
